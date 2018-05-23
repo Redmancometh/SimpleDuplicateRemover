861107b165066aa7a120533decafd4dcf0f0ad04
Redmancometh <Brendanstcurry@gmail.com>
2018-05-23 00:10:17 -0500

    Added apache commons, sped shit up a bunch, not bothering with maven

--- src/com/redmancometh/wipedupes/DupeWipe.java
+++ src/com/redmancometh/wipedupes/DupeWipe.java
@@ -2,58 +2,55 @@ package com.redmancometh.wipedupes;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.AccessDeniedException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.concurrent.CopyOnWriteArrayList;
+
+import org.apache.commons.codec.digest.DigestUtils;
 
 public class DupeWipe {
-	private static List<Path> deleted = new CopyOnWriteArrayList();
 	public static void main(String[] args) {
 		File f = new File("cache");
 		Path root = Paths.get(f.getAbsolutePath());
 		try {
-			Files.walk(root).filter((newPath) -> !Files.isDirectory(newPath)).forEach((path) -> {
-				try {
-					if (!deleted.contains(path)) {
-						System.out.println(path);
-						byte[] bytes = MessageDigest.getInstance("MD5").digest(Files.readAllBytes(path));
-						checkDuplicatesFor(path, bytes);
-					}
-				} catch (NoSuchAlgorithmException | IOException e) {
-					e.printStackTrace();
+			Files.walk(root).parallel().filter((newPath) -> !Files.isDirectory(newPath)).forEach((path) -> {
+				try (InputStream in = Files.newInputStream(path)) {
+					System.out.println(path);
+					checkDuplicatesFor(path, DigestUtils.md5Hex(in));
+				} catch (IOException e) {
+					if (e instanceof AccessDeniedException) {
+						System.out.println("Access denied are you fucking kidding me?");
+						System.out.println("Path: " + path);
+					} else
+						e.printStackTrace();
 				}
+
 			});
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 
-	public static void checkDuplicatesFor(Path toCheck, byte[] bytes) throws IOException {
+	public static void checkDuplicatesFor(Path toCheck, String hash) throws IOException {
 		File f = new File("cache");
 		Path root = Paths.get(f.getAbsolutePath());
-		Files.walk(root).filter((newPath) -> !Files.isDirectory(newPath)).forEach((path) -> {
-			byte[] otherBytes;
+		Files.walk(root).parallel().filter((newPath) -> !Files.isDirectory(newPath)).forEach((path) -> {
 			try {
-				otherBytes = MessageDigest.getInstance("MD5").digest(Files.readAllBytes(path));
-				if ((!deleted.contains(path)) && path.toAbsolutePath().equals(toCheck) && Arrays.equals(bytes, otherBytes)) {
+				String otherHash = DigestUtils.md5Hex(Files.newInputStream(path));
+				if ((!toCheck.toString().equals(path.toString())) && hash.equals(otherHash)) {
 					System.out.println("found duplicate!");
-					try {
-						deleted.add(path);
-						Files.delete(path);
-					} catch (IOException e) {
-						e.printStackTrace();
-					}
+					Files.delete(path);
 				}
-			} catch (NoSuchAlgorithmException | IOException e1) {
-				e1.printStackTrace();
-			}
 
+			} catch (IOException e) {
+				if (e instanceof AccessDeniedException) {
+					System.out.println("Access denied (INNER!) are you fucking kidding me?");
+					System.out.println("Path: " + path);
+				} else
+					e.printStackTrace();
+			}
 		});
 	}
 }
